a2e6e7706f2488667ffebe80b89b9cad3752c91f
kenji-yt <kenjiyiptong@gmail.com>
2026-02-02 15:05:44 +0100

    suecica validation done

--- A.suecica_validation.R
+++ A.suecica_validation.R
@@ -1,14 +1,22 @@
+# Assumes the working directory is 'HEAL_Paper_Validation_Analysis' 
+
 #################
 # Load Packages #
 #################
 
 library(healr)
 
+###############
+# Set threads #
+###############
+
+nThreads <- 10 # Edit to fit your resources 
+
 ####################################
 # Load data and rename chromosomes #
 ####################################
 
-final_list <- read_heal_list("/srv/kenlab/kenji/HE_re_analysis_published/Aled_suecica/HEAL/results/healr/healr_list/")
+final_list <- read_heal_list("data/A.suecica/healr_list/")
 
 wa <- list(final_list$A.thaliana, final_list$A.arenosa)
 names(wa) <- names(final_list)[c(2,1)]
@@ -25,27 +33,21 @@ wa$A.arenosa$bins <- data.table::setkey(wa$A.arenosa$bins, chr, start, end, gc_c
 final_list <- wa
 
 
-#########################################################
-# Try a range of filtering schemes and show the results #
-#########################################################
+#######################################
+#      Filtering & GC correction      #
+#######################################
 
-########## Supplementary figure 2. A. ###########
-# No filter local 
-filt_aled <- final_list
-cn_aled <- get_copy_number(filt_aled, n_threads = 10, method = "local", full_output =T)
-plot_bins(cn_aled, view_sample = "NAT_X_SYN_042", plot_cn = T, specific_chr = c("1", "6"), method = "local", color_map = c("red2", "blue2"), output_dir = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/diff_filter/no_filter", device = "svg")
+# Filtering
+filt_aled <- filter_bins(final_list, 
+                         mappability_threshold = 0.98, gc_threshold = 1.5)
 
-########## Supplementary figure 2. B. ###########
-# mappability = 0.99, gc = 2, counts = 2
-filt_aled <- filter_bins(final_list, mappability_threshold = 0.99, log_file = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/diff_filter/default_local/filtering.log")
-cn_aled <- get_copy_number(filt_aled, n_threads = 10, method = "local", full_output = T)
-plot_bins(cn_aled, view_sample = "NAT_X_SYN_042", plot_cn = T, specific_chr = c("1", "6"), method = "local", color_map = c("red2", "blue2"), output_dir = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/diff_filter/default_local", device = "svg")
+########## Supplementary Figure 2 ###########
+# GC correction 
+dir.create("Supp_figure_2")
+gc_corrected <- correct_gc(filt_aled, local_normalize = TRUE, n_windows = 10,
+                           loess_span = 1, pch = 16, alpha = 0.3, cex = 0.6,
+                           output_dir = "Supp_figure_2", device = "pdf")
 
-########## Supplementary figure 2. B. ###########
-# mappability = 0.8, gc = 1, counts = 1
-filt_aled <- filter_bins(final_list, mappability_threshold = 0.8, gc_threshold = 1, count_threshold = 1, log_file = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/diff_filter/stringent/filtering.log")
-cn_aled <- get_copy_number(filt_aled, n_threads = 10, method = "local", full_output = T)
-plot_bins(cn_aled, view_sample = "NAT_X_SYN_042", plot_cn = T, specific_chr = c("1", "6"), method = "local", color_map = c("red2", "blue2"), output_dir = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/diff_filter/stringent", device = "svg")
 
 ######################################
 # Get plot of local vs global median #
@@ -53,7 +55,8 @@ plot_bins(cn_aled, view_sample = "NAT_X_SYN_042", plot_cn = T, specific_chr = c(
 
 # Set to default filter
 filt_aled <- filter_bins(final_list)
-cn_aled <- get_copy_number(filt_aled, n_threads = 10, method = "local", full_output =T)
+cn_aled <- get_copy_number(filt_aled, n_threads = nThreads,
+                           method = "local", full_output =T)
 
 # Get the medians over each subgenome for each sample
 local_medians <- get_sample_stats(cn_aled, method = "local_median")
@@ -67,8 +70,8 @@ count_thal <- cn_aled$A.thaliana$bins$NAT_X_SYN_004[cn_aled$A.thaliana$bins$chr=
 pos_are <- cn_aled$A.arenosa$bins$start[cn_aled$A.arenosa$bins$chr=="6"]
 pos_thal <- cn_aled$A.thaliana$bins$start[cn_aled$A.thaliana$bins$chr=="1"]
 
-########## Figure 2 ###########
-# Plot it 
+########## Supplementary Figure 3 ###########
+
 par(mfrow=c(1,2))
 plot(pos_thal, count_thal, ylim=c(0,6000), pch=16, main="A.thaliana Subgenome Chromosome 1", ylab="Counts", xlab = "Position (bp)")
 abline(h=medians$NAT_X_SYN_004$A.arenosa, col='red', lwd=1.5)
@@ -78,26 +81,28 @@ abline(h=medians$NAT_X_SYN_004$A.arenosa, col='red', lwd=1.5)
 abline(h=local_medians$NAT_X_SYN_004$A.arenosa, col='green', lwd=3)
 
 
-#####################################
-# Settings which match Nibau et al. #
-#####################################
-
-# Filtering
-filt_aled <- filter_bins(final_list, mappability_threshold = 0.98, gc_threshold = 1.5)
-
-########## Figure 3 ###########
-# GC correction 
-gc_corrected <- correct_gc(filt_aled, local_normalize = T, n_windows = 10, loess_span = 1, pch = 16, alpha = 0.3, cex = 0.6, output_dir = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/gc_filtering_test", device = "svg", output_dir = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/gc_filtering_test", )
+#####################
+# Infer copy number #
+#####################
 
 # Copy Number
-cn_aled <- get_copy_number(gc_corrected, n_threads = 10, method = "local", full_output =T)
+cn_aled <- get_copy_number(gc_corrected, n_threads = nThreads,
+                           method = "local", full_output =T)
 
 # Remove short spans
 wa <- remove_short_spans(cn_aled, max_length = 3)
 
-########## Figure 4. B. ###########
+
+#########################
+#     Plot all bins     #
+#########################
+
+########## Figure 2. B. ###########
 # Plot all bins
+dir.create("figure2")
 plot_all_bins(heal_list = wa, plot_cn = T, method = "local", sample_name_size = 4,  
               view_samples =c("NAT_X_SYN_004", "NAT_X_SYN_051","NAT_X_SYN_042","NAT_X_SYN_054", "NAT_X_SYN_044"),
                 color_map = c("red2", "blue2"),
-              add_bins = T, bin_point_size = 1.3, bin_point_alpha = 0.1, output_dir = "/srv/kenlab/kenji/exploring_aled_BMC_genomics_paper/", width = 10, height = 8, device = "svg")
+              add_bins = T, bin_point_size = 1.3, bin_point_alpha = 0.1,
+              output_dir = "figure2", width = 10, height = 8, 
+              device = "png")
